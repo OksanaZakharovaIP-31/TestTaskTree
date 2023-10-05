eb418cf0772fe1215ffcaf342ed895d7fdbbc7c6
Oksana <oksana2000-02@mail.ru>
2023-10-05 22:02:18 +0300

    create database, add db in admin panel, add few line in db

--- TreeApp/App/models.py
+++ TreeApp/App/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=300,
+                            blank=True,
+                            null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=300)
+    category = models.ForeignKey(MenuCategory,
+                                 on_delete=models.CASCADE,
+                                 blank=True,
+                                 null=False)
+    parent = models.ForeignKey('self',
+                               on_delete=models.SET_DEFAULT,
+                               blank=True,
+                               null=True,
+                               default=None)
+    path = models.CharField(max_length=1000,
+                            blank=True,
+                            null=False)
+
+    def __str__(self):
+        return self.name
--- TreeApp/TreeApp/settings.py
+++ TreeApp/TreeApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9lg*v-qgme&ppfyyv&do-!rmjph+)kky*7s$52m(95dwo&828_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'TreeApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'App',
+        'USER': os.envirion['USER_DB'],
+        'PASSWORD': os.envirion['PASSWORD_DB'],
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
